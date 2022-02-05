5c40f2c97b4f4d659ff95515a9f37fcea814e584
Chris <chris.gr.skinner@gmail.com>
2022-02-05 13:35:09 +0000

    country dummy data and console.py added

---
+++ console.py
@@ -0,0 +1,7 @@
+import pdb
+from models.country import Country
+from models.city import City
+
+import repositories.country_repository as country_repository
+
+country1= Country('Scotland', ' Europe', 'https://www.pexels.com/photo/a-man-wearing-red-jacket-doing-peace-sign-3225529/', 'Haggis hunting', 'No haggis found but met Nessie')
