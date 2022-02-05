d8c1c394024ecae534bfdf7b1243712d85de80af
Chris <chris.gr.skinner@gmail.com>
2022-02-05 11:19:03 +0000

    create countries controller completed

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -13,9 +13,19 @@ def countries():
 
 # NEW
 # GET
-@countries_blueprint.route("/countries/new", methods=["GET"])
+@countries_blueprint.route("/countries/new", methods=['GET'])
 def new_country():
     countries = country_repository.select_all()
     return render_template("countries/new.html", all_countries = countries)
 
 # POST
+@countries_blueprint.route("/countries", methods=['POST'])
+def create_country():
+    country_name = request.form['country_name']
+    continent = request.form['continent']
+    img_url = request.form['img_url']
+    reason = request.form['reason']
+    reflection = request.form['refelction']
+    country = Country(country_name, continent, img_url, reason, reflection)
+    country_repository.new_country(country)
+    return redirect('/countries')
