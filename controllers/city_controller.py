273102ecb8e7f9f3ae51cd0b9b9d3a89ddfe7143
Chris <chris.gr.skinner@gmail.com>
2022-02-06 15:58:34 +0000

    cities new created

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -7,6 +7,7 @@ import repositories.city_repository as city_repository
 
 cities_blueprint = Blueprint("cities", __name__)
 
+# SHOW ALL
 @cities_blueprint.route("/cities")
 def cities():
     cities = city_repository.select_all()
@@ -17,3 +18,23 @@ def cities():
 def select(id):
     city = city_repository.select(id)
     return render_template("cities/show.html", city = city)
+
+    # CREATE CITY
+# GET
+@cities_blueprint.route("/cities/new", methods=['GET'])
+def new_city():
+    cities = city_repository.select_all()
+    return render_template("cities/new.html", cities = cities)
+
+# POST
+@cities_blueprint.route("/cities", methods=['POST'])
+def create_city():
+    city_name = request.form['city_name']
+    country_id = request.form['continent']
+    img_url = request.form['img_url']
+    visited = request.form['visited']
+    reason = request.form['reason']
+    reflection = request.form['reflection']
+    city = City(city_name, country_id, img_url, visited, reason, reflection)
+    city_repository.new_city(city)
+    return redirect('/cities')
