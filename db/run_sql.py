23fe3e0ea9b2d3358357bff5e42446b55dcf6332
james1miller93 <james1miller93@icloud.com>
2023-11-07 20:07:01 +0000

    adding docker compose file and postgres database

--- db/run_sql.py
+++ db/run_sql.py
@@ -6,7 +6,12 @@ def run_sql(sql, values = None):
     results = []
     
     try:
-        conn=psycopg2.connect("dbname='bucket_list'")
+        conn=psycopg2.connect(
+            dbname='bucket_list',
+            user='postgres',
+            password='password',
+            host='database'
+        )
         cur = conn.cursor(cursor_factory=ext.DictCursor)   
         cur.execute(sql, values)
         conn.commit()
