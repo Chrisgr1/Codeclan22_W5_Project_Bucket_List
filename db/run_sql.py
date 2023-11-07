59691a8b534f224dff9567f26e752b0d12291a24
james1miller93 <james1miller93@icloud.com>
2023-11-07 23:16:15 +0000

    obfuscate database password

--- db/run_sql.py
+++ db/run_sql.py
@@ -1,16 +1,31 @@
 import psycopg2  
 import psycopg2.extras as ext
+from os import environ
 
 def run_sql(sql, values = None):
     conn = None
     results = []
-    
+
+    if 'POSTGRES_HOST' in environ:
+        pg_host = environ['POSTGRES_HOST']
+
+    if 'POSTGRES_PASSWORD_FILE' in environ:
+        with open(environ['POSTGRES_PASSWORD_FILE']) as f:
+            pg_password = f.read()
+            f.close()
+
+    if 'POSTGRES_USER' in environ:
+        pg_user = environ['POSTGRES_USER']
+
+    pg_dbname = 'bucket_list'
+
+
     try:
         conn=psycopg2.connect(
-            dbname='bucket_list',
-            user='postgres',
-            password='password',
-            host='database'
+            host=pg_host,
+            password=pg_password,
+            user=pg_user,
+            dbname=pg_dbname
         )
         cur = conn.cursor(cursor_factory=ext.DictCursor)   
         cur.execute(sql, values)
---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO countries (id, country_name, continent, img_url, reason, reflection)
+VALUES (1, 'Scotland', 'Europe', 'https://images.pexels.com/photos/3225529/pexels-photo-3225529.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'Haggis hunting', 'No haggis found but met Nessie');
+INSERT INTO countries (id, country_name, continent, img_url, reason, reflection)
+VALUES (2, 'Vietnam', 'Asia', 'https://images.pexels.com/photos/1660996/pexels-photo-1660996.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 'Uong cafe', 'Dep');
+INSERT INTO countries (id, country_name, continent, img_url, reason, reflection)
+VALUES (3, 'Zootopia', 'Africa', 'https://i.ibb.co/4TssDvT/Whats-App-Image-2022-02-09-at-12-36-53.jpg', 'Zootopia is a huge country because its mountains fell down', 'Dep');
+
+INSERT INTO cities (id, city_name, country_id, img_url, visited, reason, reflection)
+VALUES (1, 'Glasgow', 1, 'https://images.pexels.com/photos/5870360/pexels-photo-5870360.jpeg?cs=srgb&dl=pexels-sinitta-leunen-5870360.jpg&fm=jpg', True, 'Mad choons and chips & vinegar', 'Got pure mad wi it');
+INSERT INTO cities (id, city_name, country_id, img_url, visited, reason, reflection)
+VALUES (2, 'Zootropolis', 1, 'https://i.ibb.co/VYn4TPX/Whats-App-Image-2022-02-09-at-12-36-53.jpg', False, 'Zootropolis is a city that has been buried underneath a mountain.', '');
