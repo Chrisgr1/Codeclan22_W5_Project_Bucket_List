1eb2595874861f7d90e91b1cb7340d18fcda60ef
Chris Skinner <95317377+Chrisgr1@users.noreply.github.com>
2023-11-26 18:24:08 +0000

    removed hardcoded credentials in database connection run_sql.py

--- db/run_sql.py
+++ db/run_sql.py
@@ -1,17 +1,32 @@
 import psycopg2  
 import psycopg2.extras as ext
+from os import environ
 
 def run_sql(sql, values = None):
     conn = None
     results = []
     
+    if 'POSTGRES_HOST' in environ:
+        pg_host = environ['POSTGRES_HOST']
+
+    if 'POSTGRES_PASSWORD_FILE' in environ:
+        with open(environ['POSTGRES_PASSWORD_FILE']) as f:
+            pg_password = f.read()
+            f.close()
+
+    if 'POSTGRES_USER' in environ:
+        pg_user = environ['POSTGRES_USER']
+
+    pg_dbname = 'bucket_list'
+
+
+
     try:
-        conn=psycopg2.connect("dbname='bucket_list'")
         conn=psycopg2.connect(
-            dbname='bucket list',
-            user='postgres',
-            password='password',
-            host='databsae'
+            dbname= pg_dbname,
+            user= pg_user,
+            password = pg_password,
+            host = pg_host
         )
         cur = conn.cursor(cursor_factory=ext.DictCursor)   
         cur.execute(sql, values)
